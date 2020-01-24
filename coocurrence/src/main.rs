857761befa33008564de475edd3b528fee23e568
Lu Zhang <luz20@pitt.edu>
2020-01-24 00:41:53 -0500

    run coocurrence_words on sherlockholmes.txt

--- coocurrence/src/main.rs
+++ coocurrence/src/main.rs
@@ -6,8 +6,8 @@ mod coocurrence_words;
 
 
 fn main() {
-	let data = fs::read_to_string("test.txt").expect("Unable to read file");
+	let data = fs::read_to_string("SherlockHolmes.txt").expect("Unable to read file");
 	let data = data.trim();
-//	coocurrence_words::coocurrence_all(&data.to_string(), vec!["Sherlock".to_string(), "Watson".to_string(), "said".to_string()], 10000);
-	coocurrence_char::coocurrence_all(&data.to_string(), vec!['A', 'B', 'C'], 10000);
+	coocurrence_words::coocurrence_all(&data.to_string(), vec!["Sherlock".to_string(), "Watson".to_string(), "said".to_string()], 10000);
+//	coocurrence_char::coocurrence_all(&data.to_string(), vec!['A', 'B', 'C'], 10000);
 }
